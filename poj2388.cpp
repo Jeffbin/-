4e4a6684a4c77a900913937bc6798ac62b30312a
Jeffbin <81847424+Jeffbin@users.noreply.github.com>
2022-06-15 21:56:05 +0800

    Add files via upload

---
+++ poj2388.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[10000];
+	int n;
+	int um;
+	int m;
+	cin >> n;//ÊýÁ¿
+	memset(a, 0, sizeof(a));
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m;
+		a[i] = m;
+	}
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j < n - i; j++)
+		{
+			if (a[j] > a[j+1])
+			{
+				um = a[j + 1];
+				a[j + 1] = a[j];
+				a[j] = um;
+			}
+		}
+	}
+	
+	
+	cout << a[n/2];
+	
+}
