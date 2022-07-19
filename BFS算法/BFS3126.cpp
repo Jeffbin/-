79438e58c2758031113c6bf1d00015ee348a28c9
Jeffbin <81847424+Jeffbin@users.noreply.github.com>
2022-07-19 17:36:51 +0800

    Add files via upload

--- BFS算法/BFS3126.cpp
+++ BFS算法/BFS3126.cpp
@@ -2,51 +2,60 @@
 * 问题描述：输入两个四位数的素数，求从第一个素数转化成第二个素数所花费的时间，转换的过程中全程都只能是素数，如果不可以的话，输出impossible
 * 提交状态： AC
  *
-* 交流结果： 内存： 时间：
+* 交流结果： 内存：1064K 时间：16ms
  *
 * 解题方法1：从个位开始，从0到9变，如果新组成的数如果是素数且没有遍历过，就把这个数入队，费用加1。如此操作直到千位。
 * 剪枝：个位数为0时不是素数，减去；千位数不能为0，减去
  *
 * 解题方法2 （可选）：
  *
-* 出错次数：1
+* 出错次数：2
  *
 * 错误原因 （可选）：超时，判断素数时，从2开始遍历到自身，导致超时
+* 判断素数的方法出错或超时
  *
-* 心得体会 （可选）：判断一个素数时是不是素数时，其实数字x的因数分成两大部分，
-一部分是小于x的平方根，另外一部分大于x的平方根，小于平方根和大于平方根的部分是一一对应的，
-因而可以只判断从2到平方根的数字是否都能被整除即可。
+* 心得体会 （可选）：判断一个素数时是不是素数时，可以用数学上总结的判断素数方法，节省时间的同时也正确
 这样就可以节约时间
  */
 
-#include <iostream>
-#include <queue>
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<cmath>
+#define maxn 100000
 using namespace std;
-int used[10000];
-int prime[10000];
+int used[maxn];
+int prime[maxn];
+int flag = 0;
+int pre[maxn];
 struct point
 {
 	int x;
 	int cost;
 };
-queue<point> r;
-bool sushu(int n)
-{
-	for (int j = 2; j < sqrt(n); j++)
-	{
-		if (n % j == 0)
-		{
-			prime[n] = 0;
-			return false;
-		}
 
+
+void init() {//线性筛法（O（n）） 
+	for (int i = 2; i < maxn; i++) {
+		prime[i] = true;
 	}
-	prime[n] = 1;
-	return true;
-
+	prime[0] = prime[1] = false;
+	int total = 0;
+	for (int i = 2; i < maxn; i++) {
+		if (prime[i]) {
+			pre[total++] = i;
+		}
+		for (int j = 0; j < total && i * pre[j] < maxn; j++) {
+			prime[i * pre[j]] = false;
+			if (i % pre[j] == 0) {
+				break;
+			}
+		}
+	}
 }
 void BFS(int n, int m)
 {
+	queue<point> r;
 	point start;
 	start.x = n;
 	start.cost = 0;
@@ -56,13 +65,14 @@ void BFS(int n, int m)
 		int dx = r.front().x;
 		if (dx == m)
 		{
+			flag = 1;
 			cout << r.front().cost << endl;
 			return;
 		}
-		for (int i = 1; i < 10; i++)//个位
+		for (int i = 1; i < 10; i=i+2)//个位
 		{
 			int kt = dx / 10 * 10 + i;
-			if (sushu(kt) && used[kt] == 0)
+			if (prime[kt] && used[kt] == 0)
 			{
 				point temp;
 				temp.x = kt;
@@ -74,7 +84,7 @@ void BFS(int n, int m)
 		for (int i = 0; i < 10; i++)//十位
 		{
 			int kt = dx -dx%100/10*10 + i*10;
-			if (sushu(kt) && used[kt] == 0)
+			if (prime[kt] && used[kt] == 0)
 			{
 				point temp;
 				temp.x = kt;
@@ -86,7 +96,7 @@ void BFS(int n, int m)
 		for (int i = 0; i < 10; i++)//百位
 		{
 			int kt = dx-dx % 1000/100*100 + i*100;
-			if (sushu(kt) && used[kt] == 0)
+			if (prime[kt] && used[kt] == 0)
 			{
 				point temp;
 				temp.x = kt;
@@ -98,7 +108,7 @@ void BFS(int n, int m)
 		for (int i = 1; i < 10; i++)//千位
 		{
 			int kt =dx- dx / 1000 * 1000 + i*1000;
-			if (sushu(kt) && used[kt] == 0)
+			if (prime[kt] && used[kt] == 0)
 			{
 				point temp;
 				temp.x = kt;
@@ -113,23 +123,29 @@ void BFS(int n, int m)
 int main()
 {
 	int a,b,c;
+	init();
 	cin >> a;
 	for (int op = 0; op < a; op++)
 	{
 		memset(used, 0, sizeof(used));
 		cin >> b >> c;
-		BFS(b, c);
-		if (prime[b]==0 || prime[c]==0)
+		used[b] = 1;
+		if (prime[b] == 1 && prime[c] == 1)
+		{
+			BFS(b, c);
+		}
+		if (flag == 0)
 		{
 			cout << "Impossible" << endl;
 		}
+		flag = 0;
 	}
 
 }
 /*
 Sample Input
 
-4
+3
 1033 8179
 1373 8017
 1033 1033
@@ -139,4 +155,7 @@ Sample Output
 6
 7
 0
+*/
+/*
+
 */
